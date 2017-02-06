1ec0d042c2ebd4001a84f2ae1912345ef4b5bb94
Junior Oliveira <arojunior@gmail.com>
2017-02-06 16:11:14 -0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import 'offline-js/offline.min'
+import axios from 'axios'
+
+class Request {
+    constructor() {
+        Offline.options = {
+            interceptRequests: false,
+            requests: false
+        }
+
+        Offline.on('up', () => {
+          this.checkPendingRequests()
+        })
+
+        this.checkPendingRequests()
+
+        this.eRequests = []
+    }
+
+    checkPendingRequests() {
+        let eRequests = localStorage.getItem("eRequests")
+
+        if ((eRequests !== null)&&(eRequests !== 'undefined')) {
+
+            this.eRequests = JSON.parse(eRequests)
+
+            return this.sendPendingRequests()
+        }
+
+        return false
+    }
+
+    sendPendingRequests() {
+        let requests = this.eRequests
+        let sendRequest = (item) => axios(item)
+
+        if (requests.length === 0) {
+            return false
+        }
+
+        axios.all(requests.map((item) => {
+            requests.splice(requests.indexOf(item), 1)
+            return sendRequest(item)
+        }))
+        .then(() =>  {
+            this.eRequests = requests
+            this.updateFailedRequests()
+        })
+        .catch(() => false)
+
+        return true
+    }
+
+    queueFailedRequests(requestData) {
+        this.eRequests.push(requestData)
+
+        this.updateFailedRequests()
+    }
+
+    updateFailedRequests() {
+        localStorage.setItem("eRequests", JSON.stringify(this.eRequests))
+    }
+
+    send(request, data) {
+
+        if (typeof request !== 'object') {
+            request = {
+                method  : 'post',
+                url     : request,
+                data    : data
+            }
+        }
+
+        return axios(request)
+               .catch(() => this.queueFailedRequests(request))
+    }
+}
+
+export default Request
